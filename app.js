2b3ba6d0a93df96697529c7cb26150a5f6c6fb93
Shivank Rastogi <63206129+Shivi-16@users.noreply.github.com>
2023-02-12 07:48:20 +0530

    Added Expiry to Redis Entries

--- app.js
+++ app.js
@@ -24,36 +24,39 @@ app.post("/fibonacci", async (req, res) => {
     let number = parseInt(req.body.number);
     fibonacci = (number==0)?(fibonacci = []):((number==1)?(fibonacci = [0]):(fibonacci = [0,1]));
     if(number>=2){
-    let redisKey = String(number);
-    const getAsync = promisify(client.get).bind(client);
-    const setAsync = promisify(client.set).bind(client);
-    const cachedfibonacci =  await getAsync(redisKey);
-    if(cachedfibonacci)
-    {
-        fibonacci  = JSON.parse(cachedfibonacci);
-    }else{
-    let closestComputedNumb = redisKey-1;
-    let closestFibonacci;
-    while(!closestFibonacci && closestComputedNumb>0)
-    {
-       closestFibonacci = await getAsync(closestComputedNumb);
-       closestComputedNumb--;
-    }
-    let fibonacciHelper = [0,1];
-    if(closestFibonacci)
-    {
-        fibonacciHelper = JSON.parse(closestFibonacci);
-    }
-    fibonacci = computeFibonacciSeries(number,fibonacciHelper); 
-    let stringifyFibonacci = JSON.stringify(fibonacci);
-    await setAsync(redisKey,stringifyFibonacci);
-    }
+        let redisKey = String(number);
+        const getAsync = promisify(client.get).bind(client);
+        const setAsync = promisify(client.setEx).bind(client);
+        const cachedfibonacci =  await getAsync(redisKey);
+        if(cachedfibonacci)
+        {
+            console.log("Reading from Cache");
+            fibonacci  = JSON.parse(cachedfibonacci);
+        }else{
+            let closestComputedNumb = redisKey-1;
+            let closestFibonacci;
+            while(!closestFibonacci && closestComputedNumb>0)
+            {
+                closestFibonacci = await getAsync(closestComputedNumb);
+                closestComputedNumb--;
+            }
+            console.log("Closest Smaller number from which we generate fibonacci:",closestComputedNumb+1);
+            let fibonacciHelper = [0,1];
+            if(closestFibonacci)
+            {
+                fibonacciHelper = JSON.parse(closestFibonacci);
+            }
+            fibonacci = computeFibonacciSeries(number,fibonacciHelper); 
+            let stringifyFibonacci = JSON.stringify(fibonacci);
+            await setAsync(redisKey,3600,stringifyFibonacci);
+        }
     }
     res.status(200).send(fibonacci);
 });
 
 function computeFibonacciSeries(number,fibonacciHelper)
 {
+    console.log("Fibonacci Helper",fibonacciHelper);
     let fibonacci = fibonacciHelper;
     while(fibonacci.length<number){
         fibonacci.push(parseInt(fibonacci.slice(fibonacci.length-2,fibonacci.length-1)) + parseInt(fibonacci.slice(fibonacci.length-1,fibonacci.length)))
